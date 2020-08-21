7986cc76503b7b984b90a74f3145a7d82ce595e6
Jmzer <ge1145370834@qq.com>
2020-08-21 23:11:16 +0800

    Add registration module part2. Modify the result of registering the user, add a contextprocessors and add session to myproject.  --Jmzer

--- userapp/views.py
+++ userapp/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
-
 from userapp.models import UserInfo
+from utils.code import gene_code
 
 
 class RegisterView(View):
@@ -41,3 +41,51 @@ class CheckUnameView(View):
 class CenterView(View):
     def get(self,request):
         return render(request,'center.html')
+
+
+class LogoutView(View):
+    def post(self, request):
+        #删除session中登录用户信息
+        if 'user' in request.session:
+            del request.session['user']
+
+        return JsonResponse({'delflag':True})
+
+
+class LoginView(View):
+    def get(self, request):
+
+        return render(request, 'login.html')
+
+    def post(self, request):
+        # 1.获取请求参数
+        uname = request.POST.get('uname', '')
+        pwd = request.POST.get('pwd', '')
+
+        # 2.查询数据库中是否存在
+        userList = UserInfo.objects.filter(uname=uname, pwd=pwd)
+
+        if userList:
+            request.session['user'] = userList[0]
+            return HttpResponseRedirect('/user/center/')
+        return HttpResponseRedirect('/user/login/')
+
+class LoadCodeView(View):
+    def get(self, request):
+        img, str = gene_code()
+        request.session['sessionCode'] = str
+        return HttpResponse(img, content_type='image/png')
+
+
+class CheckCodeView(View):
+    def get(self,request):
+        #获取输入框中的验证码
+        code = request.GET.get('code','')
+
+        #获取生成的验证码
+        sessionCode = request.session.get('sessionCode',None)
+
+        #比较是否相等
+        flag = code == sessionCode
+
+        return JsonResponse({'checkFlag':flag})
